b92cdc803e0fb032fbbe252f444b6d69e611bc65
Gamearoo Codes <gamearoo10@gmail.com>
2022-07-27 21:59:03 -0400

    Create DiscordReadyEvent.cs

---
+++ Events/DiscordReadyEvent.cs
@@ -0,0 +1,38 @@
+﻿using BetterBot.Utils;
+using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterBot.Events
+{
+    [PreInitialize]
+    public class DiscordReadyEvent
+    {
+        private readonly DiscordSocketClient ram;
+
+        public DiscordReadyEvent(DiscordSocketClient client)
+        {
+            client.Ready += Client_Ready;
+
+            ram = client;
+        }
+
+        public async Task Client_Ready()
+        {
+            await ram.SetGameAsync($"/help | {BotConfig.VERSION}");
+            await ram.SetStatusAsync(UserStatus.Idle);
+
+            Console.WriteLine("Ready");
+
+
+            
+
+
+            
+        }
+    }
+}
