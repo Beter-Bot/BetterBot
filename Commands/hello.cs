6974593eb78bffa546ded715a6d5819e3e6b44a1
Gamearoo Codes <gamearoo10@gmail.com>
2022-07-27 21:59:00 -0400

    Create hello.cs

---
+++ Commands/hello.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterBot.Commands
+{
+    public class hello : ModuleBase
+    {
+        private readonly DiscordSocketClient client;
+        private readonly CommandService commands;
+
+        public hello(DiscordSocketClient bot, CommandService command)
+        {
+            this.client = bot;
+            this.commands = command;
+        }
+
+        [Command("hello"), Alias("hi")]
+        [Summary("get a hello")]
+        public async Task helloAsync()
+        {
+
+            
+
+            await ReplyAsync($"Hello");
+            return;
+        }
+    }
+}
